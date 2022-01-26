d093150f5d5ba10f22380b969c12f26e5cb86718
VladislavAnanyev <a.vlad.v@yandex.ru>
2022-01-26 21:57:14 +0300

    exchange update

--- src/main/java/com/example/mywebquizengine/config/ServerConfig.java
+++ src/main/java/com/example/mywebquizengine/config/ServerConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ServerConfig {
 
-    /*@Bean
+/*@Bean
     public ServletListenerRegistrationBean<HttpSessionListener> listenerRegistrationBean() {
         ServletListenerRegistrationBean<HttpSessionListener> bean =
                 new ServletListenerRegistrationBean<>();
