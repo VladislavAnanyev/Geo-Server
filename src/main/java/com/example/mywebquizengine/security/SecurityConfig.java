65a18e69566bff5d3beb3eccb11930e57290b798
VladislavAnanyev <a.vlad.v@yandex.ru>
2022-06-21 00:19:32 +0300

    phone fix

--- src/main/java/com/example/mywebquizengine/security/SecurityConfig.java
+++ src/main/java/com/example/mywebquizengine/security/SecurityConfig.java
@@ -109,7 +109,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .antMatchers("/api/register", "/api/jwt", "/img/**",
                             "/api/quizzes", "/api/signin", "/api/googleauth", "/api/signup",
                             "/api/user/check-username", "/api/user/send-change-password-code",
-                            "/api/signup/phone",
+                            "/api/signup/phone","/api/signin/phone",
                             "/api/user/verify-password-code", "/api/user/password").permitAll()
 
                     .anyRequest().authenticated()
