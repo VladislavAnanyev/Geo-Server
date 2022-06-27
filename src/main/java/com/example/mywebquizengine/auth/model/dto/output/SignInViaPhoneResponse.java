1afc40a53ed01d7462edf080b945b993dc9256e1
VladislavAnanyev <a.vlad.v@yandex.ru>
2022-06-27 15:04:47 +0300

    bugfix

--- src/main/java/com/example/mywebquizengine/auth/model/dto/output/SignInViaPhoneResponse.java
+++ src/main/java/com/example/mywebquizengine/auth/model/dto/output/SignInViaPhoneResponse.java
@@ -4,11 +4,15 @@ import com.example.mywebquizengine.common.common.SuccessfulResponse;
 import com.example.mywebquizengine.auth.model.dto.output.AuthPhoneResponse;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
 @Accessors(chain = true)
 @AllArgsConstructor
+@NoArgsConstructor
 public class SignInViaPhoneResponse extends SuccessfulResponse {
     private AuthPhoneResponse result;
 }
